1c2b007ca5c948a5c81d62e101a99c57eb3bd63b
syun <konnnichiha0501@gmail.com>
2019-02-26 15:27:34 +0900

    nazo

--- besyari.js
+++ besyari.js
@@ -1,1 +1,21 @@
 window.alert('今日も頑張ろう！')
+
+$(function() {
+    headerAdjust('.header');
+    $(window).on('scroll', function() {
+        headerAdjust('.header');
+    });
+});
+/*なぞ*/
+// ヘッダーのサイズ調整
+var adjustPoint = 20;
+var adjustClass = 'fix';
+function headerAdjust(tgt) {
+    var scrollTop = $(window).scrollTop();
+    if(scrollTop > adjustPoint) {
+        $(tgt).addClass(adjustClass);
+    } else {
+        $(tgt).removeClass(adjustClass);
+    }
+}
+/*なぞ*/
